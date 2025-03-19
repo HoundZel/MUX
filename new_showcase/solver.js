1948a5c7e353972ecb1e5087ebc89d29a6773c74
HoundZel <wilsonpoon36@gmail.com>
2025-03-19 20:18:31 +0800

    smol stuff

--- new_showcase/solver.js
+++ new_showcase/solver.js
@@ -9,7 +9,7 @@ function parseBooleanEquation(equation, variableValues) {
     // console.log("Variable Values:", variableValues);
 
     const precedence = {
-        '(': 0,  // Lowest precedence for brackets
+        '(': 0,  // higest piority for brackets
         '+': 1,  // OR
         '⊕': 2,  // XOR
         '⊖': 2,  // XNOR
